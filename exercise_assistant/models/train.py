bfac9135db5128a7db69b46d2eb58c436c307451
moonbeam5115 <fabritecht@gmail.com>
2021-08-13 11:51:22 -0400

    update preprocessing.py and add train.py and create.py modules

---
+++ exercise_assistant/models/train.py
@@ -0,0 +1,53 @@
+import numpy as np
+import os
+from sklearn.model_selection import train_test_split
+from tensorflow.keras import callbacks
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.callbacks import TensorBoard
+
+
+def build_lstm(frames_per_video, keypoints_per_frame, actions):
+    model = Sequential()
+    model.add(LSTM(64, return_sequences=True, activation='relu',
+                input_shape=(frames_per_video, keypoints_per_frame)))
+    model.add(LSTM(128, return_sequences=True, activation='relu'))
+    model.add(LSTM(64, return_sequences=False, activation='relu'))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dense(actions.shape[0], activation='softmax'))
+    model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+
+    return model
+
+def train_model(model):
+
+    model.fit(X_train, y_train, epochs=350, callbacks=[tb_callback])
+
+    return model
+
+def save_model(model, dest_directory):
+    out_file_path = os.path.join(dest_directory, 'exercise_recognition_model.h5')
+    model.save(out_file_path)
+
+if __name__ == '__main__':
+    ROOT = 'exercise_assistant'
+    log_dir = os.path.join(ROOT, 'models', 'logs')
+    tb_callback = TensorBoard(log_dir=log_dir)
+    keypoints_per_frame = 132
+    frames_per_video = 45
+    actions = np.array(['left_lunge', 'right_lunge', 'stand'])
+    
+    x_path = os.path.join(ROOT, 'models', 'processed_data', 'X', 'features.npy')
+    y_path = os.path.join(ROOT, 'models', 'processed_data', 'y', 'target.npy')
+    
+    X_data = np.load(x_path)
+    y = np.load(y_path)
+    
+    X_train, X_test, y_train, y_test = train_test_split(X_data, y, test_size=0.1, random_state=42)
+    exercise_assistant = build_lstm(frames_per_video, keypoints_per_frame, actions)
+
+    trained_assistant = train_model(exercise_assistant)
+
+    MODEL_OUT_PATH = os.path.join('exercise_assistant', 'models', 'action_recognition')
+    save_model(trained_assistant, MODEL_OUT_PATH)
--- exercise_assistant/tools/preprocessing.py
+++ exercise_assistant/tools/preprocessing.py
@@ -1,4 +1,3 @@
-from sklearn.model_selection import train_test_split
 from tensorflow.keras.utils import to_categorical
 import numpy as np
 import os
@@ -10,21 +9,36 @@ frames_per_video = 45
 actions = np.array(['left_lunge', 'right_lunge', 'stand'])
 
 label_map = {label:num for num, label in enumerate(actions)}
-
+sequences, labels = [], []
 # A sequence is a group of frames representing an action
 # Sequences are all the groups of frames representing all actions - our features AKA X (input)
 # Labels will be our target variable (true value for action) - y (output)
-sequences, labels = [], []
-for action in actions:
-    for vid_number in range(videos):
-        action_keypoints = []
-        for frame_number in range(frames_per_video):
-            frame_keypoints = np.load(os.path.join(ROOT, DATA_PATH, action, str(vid_number), "{}.npy".format(frame_number)))
-            action_keypoints.append(frame_keypoints)
-        sequences.append(action_keypoints)
-        labels.append(label_map[action])
+def create_train_test_data():
+    
+    for action in actions:
+        for vid_number in range(videos):
+            action_keypoints = []
+            for frame_number in range(frames_per_video):
+                try:
+                    frame_keypoints = np.load(os.path.join(ROOT, DATA_PATH, action, str(vid_number), "{}.npy".format(frame_number)))
+                    action_keypoints.append(frame_keypoints)
+                except Exception as e:
+                    print(e)
+            sequences.append(action_keypoints)
+            labels.append(label_map[action])
+    
+    X = np.array(sequences)
+    y = to_categorical(labels).astype(int)
 
-print(np.array(sequences).shape)
-print(len(sequences))
-# (90, 45, 132)
-# (Videos, Frames_per_video, Keypoints_per_frame)
+    X_npy_path = os.path.join(ROOT, 'models', 'processed_data', 'X', 'features')
+    y_npy_path = os.path.join(ROOT, 'models', 'processed_data', 'y', 'target')
+    np.save(X_npy_path, X)
+    np.save(y_npy_path, y)
+
+
+if __name__ == '__main__':
+    create_train_test_data()
+    print(np.array(sequences).shape)
+    print(len(sequences))
+    # (90, 45, 132)
+    # (Videos, Frames_per_video, Keypoints_per_frame)
