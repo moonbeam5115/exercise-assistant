e33659b52c990491f5b47797d85b5f429ea446e2
moonbeam5115 <fabritecht@gmail.com>
2021-08-13 23:14:37 -0400

    fix action predicted put text and add requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
 from setuptools import setup, find_packages
 
-setup(name='exercise_assistant', version='1.0', packages=find_packages())
+setup(name='exercise_assistant',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        'opencv-python==4.5.3.56',
+        'tensorflow==2.5.0',
+        'scikit-learn==0.24.2',
+        'mediapipe==0.8.3',
+        'pyttsx3==2.90'
+    ]
+)
