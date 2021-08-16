4197813f525310c75bf88d3940a2c0c3efef4990
moonbeam5115 <fabritecht@gmail.com>
2021-08-16 17:33:35 -0400

    removed commented code

--- exercise_assistant/tools/preprocessing.py
+++ exercise_assistant/tools/preprocessing.py
@@ -54,40 +54,6 @@ class CreateTrainTestData():
 
         return X_data, y
 
-# def create_train_test_data():
-#     ROOT = 'exercise_assistant'
-#     DATA_PATH = 'data'
-#     videos = 30
-#     frames_per_video = 45
-#     actions = np.array(['left_lunge', 'right_lunge', 'stand'])
-
-#     label_map = {label:num for num, label in enumerate(actions)}
-#     sequences, labels = [], []
-
-#     for action in actions:
-#         for vid_number in range(videos):
-#             action_keypoints = []
-#             for frame_number in range(frames_per_video):
-#                 try:
-#                     frame_keypoints = np.load(os.path.join(ROOT, DATA_PATH, action, str(vid_number), "{}.npy".format(frame_number)))
-#                     action_keypoints.append(frame_keypoints)
-#                 except Exception as e:
-#                     print(e)
-#             sequences.append(action_keypoints)
-#             labels.append(label_map[action])
-    
-#     # X_data is the features (List of keypoint positions + visibility) that describe the action
-#     # y is the target value (The ground truth for that action)
-#     X_data = np.array(sequences)
-#     y = to_categorical(labels).astype(int)
-
-#     X_npy_path = os.path.join(ROOT, 'models', 'processed_data', 'X', 'features')
-#     y_npy_path = os.path.join(ROOT, 'models', 'processed_data', 'y', 'target')
-#     np.save(X_npy_path, X_data)
-#     np.save(y_npy_path, y)
-
-#     return X_data, y
-
 
 if __name__ == '__main__':
     DATA_PATH = os.path.join('exercise_assistant', 'data')
