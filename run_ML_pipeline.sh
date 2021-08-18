02bbaa341234325a8d06d7591e3593d4e6fd59ba
moonbeam5115 <fabritecht@gmail.com>
2021-08-17 20:01:39 -0400

    push change

--- run_ML_pipeline.sh
+++ run_ML_pipeline.sh
@@ -1,5 +1,5 @@
-export vNum=2
-export FPV=20
+export vNum=3 # Number of Videos to Capture
+export FPV=20 # Number of Frames per video
 python exercise_assistant\\tools\\collect_data.py --videos $vNum --fpv $FPV
 python exercise_assistant\\tools\\preprocessing.py --videos $vNum --fpv $FPV
 python exercise_assistant\\models\\train.py --videos $vNum --fpv $FPV --save
